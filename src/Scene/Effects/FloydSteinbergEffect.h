75ffd526cde4dcf2abd1f9306609493127b1a1e3
Cedric Martens <cedricmartens98@gmail.com>
2023-08-25 09:01:12 -0400

    Floyd Steinberg downscaled

--- src/Scene/Effects/FloydSteinbergEffect.h
+++ src/Scene/Effects/FloydSteinbergEffect.h
@@ -50,7 +50,7 @@ public:
     }
 
     if(grayscale_pixels.size() != width * height)
-      grayscale_pixels.resize(width * height);
+      grayscale_pixels.resize(width * height / (samples_width * samples_height));
 
     if (framebuffer == nullptr || framebuffer->getWidth() != width || framebuffer->getHeight() != height) {
       framebuffer = std::make_shared<Framebuffer>(width, height, false, 1);
@@ -64,50 +64,52 @@ public:
 
 
     down_sample(pixels, down_sampler, width, height, samples_width, samples_height);
+//    //pixels_to_ppm(pixels, width, height, "buffer.ppm");
+    rgba_to_grayscale(down_sampler, grayscale_pixels);
+    int down_sampled_width = width / samples_width;
+    int down_sampled_height = height / samples_height;
+
+    for(int x = 0; x < down_sampled_width; x++)
+    {
+      for(int y = 0; y < down_sampled_height; y++)
+      {
+         int index = get_index(x, y, down_sampled_width);
+         int32_t old_pixel = grayscale_pixels[index];
+         int32_t new_pixel = 0;
+         if(old_pixel >= 128)
+           new_pixel = 255;
+         else
+           new_pixel = 0;
+
+         grayscale_pixels[index] = new_pixel;
+         int32_t error = new_pixel - old_pixel;
+         if(x < down_sampled_width - 1)
+         {
+           int right_index = get_index(x+1, y, down_sampled_width);
+           grayscale_pixels[right_index] -= static_cast<int32_t>(static_cast<float>(error) * (7.f / 16.f));
+         }
+
+         if(x < down_sampled_width - 1 && y < down_sampled_height - 1)
+         {
+           int bottom_right_index = get_index(x+1, y+1, down_sampled_width);
+           grayscale_pixels[bottom_right_index] -= static_cast<int32_t>(static_cast<float>(error) * (1.f / 16.f));
+         }
+
+         if(y < down_sampled_height - 1)
+         {
+           int bottom = get_index(x, y+1, down_sampled_width);
+           grayscale_pixels[bottom] -= static_cast<int32_t>(static_cast<float>(error) * (5.f / 16.f));
+         }
+
+         if(x > 0 && y < down_sampled_height - 1)
+         {
+           int bottom_left = get_index(x-1, y+1, down_sampled_width);
+           grayscale_pixels[bottom_left] -= static_cast<int32_t>(static_cast<float>(error) * (3.f / 16.f));
+         }
+      }
+    }
+    grayscale_to_rgba(grayscale_pixels, down_sampler);
     up_sample(down_sampler, pixels, width, height, samples_width, samples_height);
-    //pixels_to_ppm(pixels, width, height, "buffer.ppm");
-//    rgba_to_grayscale(pixels, grayscale_pixels);
-//
-//    for(int x = 0; x < width; x++)
-//    {
-//      for(int y = 0; y < height; y++)
-//      {
-//         int index = get_index(x, y, width);
-//         int32_t old_pixel = grayscale_pixels[index];
-//         int32_t new_pixel = 0;
-//         if(old_pixel >= 128)
-//           new_pixel = 255;
-//         else
-//           new_pixel = 0;
-//
-//         grayscale_pixels[index] = new_pixel;
-//         int32_t error = new_pixel - old_pixel;
-//         if(x < width - 1)
-//         {
-//           int right_index = get_index(x+1, y, width);
-//           grayscale_pixels[right_index] -= static_cast<int32_t>(static_cast<float>(error) * (7.f / 16.f));
-//         }
-//
-//         if(x < width - 1 && y < height - 1)
-//         {
-//           int bottom_right_index = get_index(x+1, y+1, width);
-//           grayscale_pixels[bottom_right_index] -= static_cast<int32_t>(static_cast<float>(error) * (1.f / 16.f));
-//         }
-//
-//         if(y < height - 1)
-//         {
-//           int bottom = get_index(x, y+1, width);
-//           grayscale_pixels[bottom] -= static_cast<int32_t>(static_cast<float>(error) * (5.f / 16.f));
-//         }
-//
-//         if(x > 0 && y < height - 1)
-//         {
-//           int bottom_left = get_index(x-1, y+1, width);
-//           grayscale_pixels[bottom_left] -= static_cast<int32_t>(static_cast<float>(error) * (3.f / 16.f));
-//         }
-//      }
-//    }
-//    grayscale_to_rgba(grayscale_pixels, pixels);
 
     colorSource->getColorAttachment(0)->bind();
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, pixels.data());
