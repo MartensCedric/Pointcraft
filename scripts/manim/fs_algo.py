574bcadb5c3e03537c3f12fa165d3c2ec2cb0858
Cedric Martens <cedricmartens98@gmail.com>
2023-08-29 21:02:25 -0400

    Add manimation

--- scripts/manim/fs_algo.py
+++ scripts/manim/fs_algo.py
@@ -38,7 +38,10 @@ def update_text_element(text_elements, coord, size, new_val):
     index = coord[1] + coord[0] * size[1]
     el = text_elements[index]
     center = el.get_center()
-    el.become(Text(str(new_val), font_size=el.font_size))
+    color = WALTZ_WHITE
+    if new_val >= 128:
+        color = WALTZ_BLACK
+    el.become(Text(str(new_val), font_size=el.font_size, color=color))
     el.shift(center)
     return el
 
@@ -174,5 +177,101 @@ class FloydSteinbergAlgorithm(Scene):
         self.add(img)
         self.wait(1)
         self.play(Uncreate(hl_rect))
+
+        self.remove(img)
+
+        self.remove(error_text)
+        error_text = Text(f"Error = {138} - {255} = {138 - 255}", font_size=36)
+        error_text.shift(4 * RIGHT)
+        self.add(error_text)
+        image_data[0, 1] = 255
+        update_text_element(text_elements, coord=(0, 1), size=(6, 6), new_val=image_data[0][1])
+        img = get_image_mobject(image_data)
+        img.shift(-2 * RIGHT)
+
+        self.remove(error_table)
+        error_table = get_error_table(-117)
+        error_table.shift(4 * RIGHT - 2 * UP)
+        error_table.scale(0.5)
+        error_table.set(width=table.width)
+        self.add(error_table)
+        self.add(img)
+
+        self.wait(3)
+
+        hl_rect = Rectangle(width=4.5, height=2.0, stroke_color=WALTZ_YELLOW)
+        hl_rect.next_to(1.5 * RIGHT - 2 * UP)
+        self.play(Create(hl_rect))
+        self.wait(1)
+
+        self.play(Uncreate(hl_rect))
+
+        hl_rect = Rectangle(width=3.5, height=2.0, stroke_color=WALTZ_YELLOW)
+        hl_rect.next_to(-5.6 * RIGHT + 2 * UP)
+        self.play(Create(hl_rect))
+        self.wait(1)
+
+        self.remove(img)
+        image_data[0, 2] += -51
+        update_text_element(text_elements, coord=(0, 2), size=(6, 6), new_val=image_data[0][2])
+        image_data[1, 2] += -7
+        update_text_element(text_elements, coord=(1, 2), size=(6, 6), new_val=image_data[1][2])
+        image_data[1, 1] += -36
+        update_text_element(text_elements, coord=(1, 1), size=(6, 6), new_val=image_data[1][1])
+        image_data[1, 0] += -21
+        update_text_element(text_elements, coord=(1, 0), size=(6, 6), new_val=image_data[1][0])
+        img = get_image_mobject(image_data)
+        img.shift(-2 * RIGHT)
+        self.add(img)
+        self.play(Uncreate(hl_rect))
+        self.wait(1)
+
+        self.play(FadeOut(error_table), FadeOut(error_text))
+
+        for i in range(2, len(image_data) * len(image_data[0])):
+            width = len(image_data[0])
+            height = len(image_data)
+            col = i % width
+            row = int(np.floor(i / height))
+
+            pix = image_data[row, col]
+            if pix >= 128:
+                pix = 255
+            else:
+                pix = 0
+            error = image_data[row, col] - pix
+            image_data[row, col] = pix
+            update_text_element(text_elements, coord=(row, col), size=(6, 6), new_val=pix)
+
+            if col + 1 < width:
+                v = int(error * (7/16))
+                image_data[row, col+1] += v
+                update_text_element(text_elements, coord=(row, col+1), size=(6, 6), new_val=image_data[row, col+1])
+
+            if col + 1 < width and row + 1 < height:
+                v = int(error * (1/16))
+                image_data[row+1, col+1] += v
+                update_text_element(text_elements, coord=(row+1, col+1), size=(6, 6), new_val=image_data[row+1, col+1])
+
+            if row + 1 < height:
+                v = int(error * (5 / 16))
+                image_data[row + 1, col] += v
+                update_text_element(text_elements, coord=(row + 1, col), size=(6, 6),
+                                    new_val=image_data[row + 1, col])
+
+            if col - 1 >= 0 and row + 1 < height:
+                v = int(error * (3 / 16))
+                image_data[row + 1, col - 1] += v
+                update_text_element(text_elements, coord=(row + 1, col - 1), size=(6, 6),
+                                    new_val=image_data[row + 1, col - 1])
+
+            self.remove(img)
+            img = get_image_mobject(image_data)
+            img.shift(-2 * RIGHT)
+            self.add(img)
+            self.wait(0.1)
+
+        print(image_data)
+
 
         self.wait(5)
