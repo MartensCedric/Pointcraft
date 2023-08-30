83f8f035eb738c92ceba000a8d036fa5358f1ab5
Cedric Martens <cedricmartens98@gmail.com>
2023-08-29 20:06:09 -0400

    Add main animation

--- scripts/manim/fs_algo.py
+++ scripts/manim/fs_algo.py
@@ -2,12 +2,24 @@ from manim import *
 import numpy as np
 from styles import *
 
+ERR_NO = 5.4 * RIGHT
+FS_R = 4.42 * RIGHT + 2.37 * UP
+FS_DR = 4.42 * RIGHT + 2.42 * UP
+FS_D = 3.5 * RIGHT + 2.5 * UP
+FS_DL = 2 * RIGHT + 1 * UP
+
+RES_R = 4.42 * RIGHT - 1.57 * UP
+RES_DR = 4.42 * RIGHT - 2.42 * UP
+RES_D = 3.5 * RIGHT - 2.5 * UP
+RES_DL = 2 * RIGHT - 1 * UP
+
 def get_image_mobject(data):
     img = ImageMobject(data)
     img.height = 6
     img.set_resampling_algorithm(RESAMPLING_ALGORITHMS["nearest"])
     img.set_z_index(-1)
     return img
+
 
 def create_text_grid(data, size, offset, font_size):
     text_elements = []
@@ -21,25 +33,42 @@ def create_text_grid(data, size, offset, font_size):
     text_v_group = VGroup(*text_elements)
     return text_v_group, text_elements
 
+
 def update_text_element(text_elements, coord, size, new_val):
-    index = coord[1] +  coord[0] * size[1]
+    index = coord[1] + coord[0] * size[1]
     el = text_elements[index]
     center = el.get_center()
     el.become(Text(str(new_val), font_size=el.font_size))
     el.shift(center)
     return el
 
+
 def get_error_table(value):
     error_table = Table(
-        [["", "*", str(int((7/16)*value))],
-        [str(int((3/16)*value)),str(int((5/16)*value)), str(int((1/16)*value))]],
-    include_outer_lines=True)
+        [["", "*", str(int((7 / 16) * value))],
+         [str(int((3 / 16) * value)), str(int((5 / 16) * value)), str(int((1 / 16) * value))]],
+        include_outer_lines=True)
     return error_table
+
+
+def highlight(scene, location, wait=0):
+    hl_square = get_circle()
+    hl_square.next_to(location)
+    scene.play(Create(hl_square))
+    scene.wait(wait)
+    scene.play(Uncreate(hl_square))
+
+
+def get_circle(radius=0.6):
+    return Circle(radius=0.6, stroke_color=WALTZ_YELLOW)
+
 
 class FloydSteinbergAlgorithm(Scene):
     def construct(self):
         Text.set_default(font="Fira Sans")
-        image_data = np.uint8([[92, 98, 90, 85, 96, 91], [85, 82, 84, 81, 82, 86], [78, 80, 72, 75, 78, 79], [68, 72, 67, 69, 70, 68], [64, 63, 67, 62, 66, 65], [58, 59, 54, 59, 57, 60]])
+        image_data = np.uint8(
+            [[92, 98, 90, 85, 96, 91], [85, 82, 84, 81, 82, 86], [78, 80, 72, 75, 78, 79], [68, 72, 67, 69, 70, 68],
+             [64, 63, 67, 62, 66, 65], [58, 59, 54, 59, 57, 60]])
         fs_weight = [["", "*", "7/16"], ["3/16", "5/16", "1/16"]]
         img_size = 6
         img = get_image_mobject(image_data)
@@ -48,17 +77,17 @@ class FloydSteinbergAlgorithm(Scene):
 
         self.wait(1)
 
-        text_v_group, text_elements = create_text_grid(image_data, size=(6,6), offset=(-2,0), font_size=36)
+        text_v_group, text_elements = create_text_grid(image_data, size=(6, 6), offset=(-2, 0), font_size=36)
         self.play(FadeIn(text_v_group))
 
         black_square = Square(fill_color=BLACK, fill_opacity=1, stroke_color=GRAY)
-        black_square.shift(4*RIGHT + 2*UP)
+        black_square.shift(4 * RIGHT + 2 * UP)
         black_square_val = Text(str(0), color=WALTZ_WHITE)
         black_square_val.move_to(black_square)
         self.play(Create(black_square), Create(black_square_val))
 
         white_square = Square(fill_color=WHITE, fill_opacity=1, stroke_color=GRAY)
-        white_square.shift(4*RIGHT - 2*UP)
+        white_square.shift(4 * RIGHT - 2 * UP)
         white_square_val = Text(str(255), color=WALTZ_BLACK)
         white_square_val.move_to(white_square)
         self.play(Create(white_square), Create(white_square_val))
@@ -71,8 +100,8 @@ class FloydSteinbergAlgorithm(Scene):
         self.play(Create(first_focus_square))
         self.play(FadeOut(first_focus_square))
 
-        image_data[0][0] = 0
-        update_text_element(text_elements, coord=(0,0), size=(6,6), new_val=0)
+        image_data[0,0] = 0
+        update_text_element(text_elements, coord=(0, 0), size=(6, 6), new_val=0)
         # print(image_data)
         # img.pixel_array = image_data
         self.remove(img)
@@ -81,12 +110,12 @@ class FloydSteinbergAlgorithm(Scene):
         self.add(img)
         self.wait(1)
         error_text = Text(f"Error = {92} - {0} = 92")
-        error_text.shift(4* RIGHT)
+        error_text.shift(4 * RIGHT)
         self.play(Create(error_text))
         self.wait(5)
         table = Table(
             [["", "*", "7/16"],
-            ["3/16","5/16", "1/16"]], include_outer_lines=True)
+             ["3/16", "5/16", "1/16"]], include_outer_lines=True)
         table.shift(4 * RIGHT + 2 * UP)
         table.scale(0.5)
 
@@ -99,10 +128,51 @@ class FloydSteinbergAlgorithm(Scene):
         error_table = get_error_table(92)
         error_table.shift(4 * RIGHT - 2 * UP)
         error_table.scale(0.5)
+        error_table.set(width=table.width)
         self.play(Create(error_table))
 
-        # fs_weights_v_group, _ = create_text_grid(fs_weight, size=(2,3), offset=(3, -2), font_size=24)
-        # self.play(FadeIn(fs_weights_v_group))
-        self.wait(5)
-        self.wait(5)
+        self.remove(img)
+        img = get_image_mobject(image_data)
+        img.shift(-2 * RIGHT)
+        self.add(img)
 
+        self.wait(1)
+        highlight(self, RES_R, 0)
+        highlight(self, FS_R, 0)
+        highlight(self, ERR_NO, 0)
+
+        highlight(self, RES_R, 0)
+
+        hl_square = get_circle()
+        hl_square.next_to(-4.36 * RIGHT + 2.55 * UP)
+        self.play(Create(hl_square))
+        self.remove(img)
+        image_data[0, 1] += 40
+        update_text_element(text_elements, coord=(0, 1), size=(6, 6), new_val=image_data[0][1])
+        img = get_image_mobject(image_data)
+        img.shift(-2 * RIGHT)
+        self.add(img)
+        self.play(Uncreate(hl_square))
+
+        hl_rect = Rectangle(width=4.5, height=1.0, stroke_color=WALTZ_YELLOW)
+        hl_rect.next_to(1.5 * RIGHT - 2.42 * UP)
+        self.play(Create(hl_rect))
+        self.wait(1)
+        self.play(Uncreate(hl_rect))
+
+        hl_rect = Rectangle(width=3.5, height=1.0, stroke_color=WALTZ_YELLOW)
+        hl_rect.next_to(-6.6 * RIGHT + 1.5 * UP)
+        self.play(Create(hl_rect))
+
+        self.remove(img)
+        image_data[1, 1] += 5
+        update_text_element(text_elements, coord=(1, 1), size=(6, 6), new_val=image_data[1][1])
+        image_data[1, 0] += 28
+        update_text_element(text_elements, coord=(1, 0), size=(6, 6), new_val=image_data[1][0])
+        img = get_image_mobject(image_data)
+        img.shift(-2 * RIGHT)
+        self.add(img)
+        self.wait(1)
+        self.play(Uncreate(hl_rect))
+
+        self.wait(5)
